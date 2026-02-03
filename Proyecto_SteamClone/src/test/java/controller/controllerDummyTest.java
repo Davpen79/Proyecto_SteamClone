b97ddb323020193fd612a46a7c885c6e8c9038f0
Davpen79 <davpenfp@gmail.com>
2026-02-03 11:58:33 +0100

    Test 2

--- Proyecto_SteamClone/src/test/java/controller/controllerDummyTest.java
+++ Proyecto_SteamClone/src/test/java/controller/controllerDummyTest.java
@@ -1,4 +1,17 @@
 package controller;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class controllerDummyTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
 }
+
