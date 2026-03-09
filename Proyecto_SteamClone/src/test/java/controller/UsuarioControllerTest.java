808ba5fabbd5a731bdac4c07a695a9109d8dbfe9
a25davidpf <davpenfp@gmail.com>
2026-03-09 16:42:50 +0100

    Configurado mockito para tests

--- Proyecto_SteamClone/src/test/java/controller/UsuarioControllerTest.java
+++ Proyecto_SteamClone/src/test/java/controller/UsuarioControllerTest.java
@@ -1,39 +1,29 @@
 package controller;
 
 import org.davpen.controller.UsuarioController;
-import org.davpen.enums.TipoEstadoCuenta;
 import org.davpen.excepciones.ValidationException;
-import org.davpen.modelo.dto.UsuarioDto;
-import org.davpen.modelo.entity.UsuarioEntity;
-import org.davpen.modelo.form.UsuarioForm;
+import org.davpen.repositorio.inmemory.UsuarioRepoInMemory;
 import org.davpen.repositorio.intefaces.IUsuarioRepo;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 public class UsuarioControllerTest {
 
-    private IUsuarioRepo usuarioRepo;
-    private UsuarioDto usuarioDto;
-    UsuarioController usuarioController = new UsuarioController(usuarioRepo);
+    @Mock
+    private IUsuarioRepo usuarioRepo = new UsuarioRepoInMemory();
+
+    @InjectMocks
+    private UsuarioController usuarioController;
 
     @Test
     public void testRegistrarUsuario_UsuarioCorrecto_UsuarioDto() throws ValidationException {
 
-        final var usuarioCorrecto = new UsuarioForm("Carlos", "carlos@mail.com", "cArlos123",
-                "carlos", "España", LocalDate.of(1950, 1, 1), LocalDate.now(),
-                "avatar", 0.5, TipoEstadoCuenta.ACTIVA);
 
-        final var usuarioRepo = new UsuarioEntity(1L, "Carlos", "carlos@mail.com",
-                "password1", "cArlos123","España", LocalDate.of(1950, 1, 1),
-                LocalDate.now(),"avatar", 0.5, TipoEstadoCuenta.ACTIVA);
 
-        final var usuarioDto = new UsuarioDto(1L, "Carlos", "carlos@mail.com",
-                "cArlos123","España", LocalDate.of(1950, 1, 1), LocalDate.now(),
-                "avatar", 0.5, TipoEstadoCuenta.ACTIVA);
-
-        assertEquals(usuarioDto, usuarioController.registrarUsuario(usuarioCorrecto));
     }
 }
