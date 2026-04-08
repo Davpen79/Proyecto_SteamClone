b59e2072fe329954584e7c372e9f3371cf8d14f1
a25davidpf <davpenfp@gmail.com>
2026-04-08 21:03:45 +0200

    Preparado el codigo para hibernate

---
+++ Proyecto_SteamClone/src/main/java/org/davpen/dbconfig/HibernateUtil.java
@@ -0,0 +1,22 @@
+package org.davpen.dbconfig;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory SESSION_FACTORY = new Configuration()
+            .configure("hibernate-h2.cfg.xml")
+            .buildSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return SESSION_FACTORY;
+    }
+
+    public static void shutdown() {
+        SESSION_FACTORY.close();
+    }
+}
